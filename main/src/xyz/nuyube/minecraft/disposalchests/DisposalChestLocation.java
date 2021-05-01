fb3e41db90e40add1017d6cc611f1d452057b6a7
Nuyube <nuyubexz@gmail.com>
2021-05-01 09:20:54 -0400

    Format DisposalChestLocation

--- main/src/xyz/nuyube/minecraft/disposalchests/DisposalChestLocation.java
+++ main/src/xyz/nuyube/minecraft/disposalchests/DisposalChestLocation.java
@@ -3,12 +3,16 @@ package xyz.nuyube.minecraft.disposalchests;
 import org.bukkit.Location;
 
 class DisposalChestLocation {
-  double x; double z; String world;
+  double x;
+  double z;
+  String world;
+
   public DisposalChestLocation(double X, double Z, String World) {
     x = X;
     z = Z;
     world = World;
   }
+
   public DisposalChestLocation(Location l) {
     x = l.getX();
     z = l.getY();
